ec0df2551405be02d5f1898c9dddcb8ec29503ef
Batuhan Bilir <bth.01srp@gmail.com>
2024-03-12 14:57:29 +0300

    obje ile ilgili kısa bir örnek

--- Javascript/Projeler/Bölüm1/Bölüm2/Object.js
+++ Javascript/Projeler/Bölüm1/Bölüm2/Object.js
@@ -3,15 +3,20 @@ let Öğrenci = {
     Yaş : 21,
     Email : "bth.01srp@gmail.com",
     Sinif: 2,
-    Dersler : ["Olasilik", "Differansiyel Denklemler", "Veri Yapıları"],
+    Dersler : [{isim: "Olasılık",puan :90},
+            {isim: "Veri Yapıları",puan :70},
+            {isim: "Differansiyel Denklemler",puan :75}],
     Logİn(){
         console.log("Öğrenci Giriş Yaptı");
     },
     LogOut(){
-        console.log("Öğrenci Çıkış Yaptı");
-}};
-
-console.log(Öğrenci.Ad);
-console.log(Öğrenci.Logİn());
+        console.log("Öğrenci Çıkış Yaptı");},
+    printLesson(){
+        this.Dersler.forEach(ders => {console.log(ders.isim,ders.puan);});
+    }
+};
+// console.log(Öğrenci.Ad);
+// console.log(Öğrenci.Logİn());
 
 //Objdeki metodu kullanarak dersleri gosterme
+console.log(Öğrenci.printLesson());
